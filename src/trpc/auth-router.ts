4a877691d4ae683af477c40b97654dc136fe1d2b
akhil_s <akhil@webyfy.com>
2023-12-24 14:54:45 +0530

    Setuped emil verification
    * Installed nodemailer
    * Installed devDependencies types/nodemailer
    * Added transporter on get-payload page
    * Added email send option on get-payload page
    * Created verify page
    * Updated verify status on users collection
    * created verify compontent

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -36,45 +36,21 @@ export const authRouter = router({
       return { success: true, sentToEmail: email }
     }),
 
-//   verifyEmail: publicProcedure
-//     .input(z.object({ token: z.string() }))
-//     .query(async ({ input }) => {
-//       const { token } = input
+   verifyEmail: publicProcedure
+     .input(z.object({ token: z.string() }))
+    .query(async ({ input }) => {
+       const { token } = input
 
-//       const payload = await getPayloadClient()
+       const payload = await getPayloadClient()
 
-//       const isVerified = await payload.verifyEmail({
-//         collection: 'users',
-//         token,
-//       })
+       const isVerified = await payload.verifyEmail({
+         collection: 'users',
+         token,
+       })
 
-//       if (!isVerified)
-//         throw new TRPCError({ code: 'UNAUTHORIZED' })
+       if (!isVerified)
+         throw new TRPCError({ code: 'UNAUTHORIZED' })
 
-//       return { success: true }
-//     }),
-
-//   signIn: publicProcedure
-//     .input(AuthCredentialsValidator)
-//     .mutation(async ({ input, ctx }) => {
-//       const { email, password } = input
-//       const { res } = ctx
-
-//       const payload = await getPayloadClient()
-
-//       try {
-//         await payload.login({
-//           collection: 'users',
-//           data: {
-//             email,
-//             password,
-//           },
-//           res,
-//         })
-
-//         return { success: true }
-//       } catch (err) {
-//         throw new TRPCError({ code: 'UNAUTHORIZED' })
-//       }
-//     }),
+       return { success: true }
+     }),
 })
